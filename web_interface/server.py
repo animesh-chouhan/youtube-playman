427a3178bc6d07b0d13617084908fc169b2fe396
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-05-15 20:29:24 +0530

    added swagger.yml

--- web_interface/server.py
+++ web_interface/server.py
@@ -7,8 +7,6 @@ app = connexion.App(__name__, specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
-# Create a URL route in our application for "/"
-
 
 @app.route('/')
 def home():
@@ -20,6 +18,5 @@ def home():
     return render_template('index.html')
 
 
-# If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
